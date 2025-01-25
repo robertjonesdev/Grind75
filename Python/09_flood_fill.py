99ef7de85f10d0205d3ea3d0ab49f90f7b1b4b4e
robertjonesdev <rtjones06@gmail.com>
2025-01-25 12:14:20 -0700

    10 and 12

--- Python/09_flood_fill.py
+++ Python/09_flood_fill.py
@@ -1,3 +1,34 @@
+from typing import List
+
+
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
+    def floodFill(
+        self, image: List[List[int]], sr: int, sc: int, color: int
+    ) -> List[List[int]]:
+        m, n = len(image), len(image[0])
+        previous = image[sr][sc]
+
+        def fill(sr, sc, color, previous):
+            if sr < 0 or sr >= m:
+                return
+            if sc < 0 or sc >= n:
+                return
+            if image[sr][sc] != previous:
+                return
+
+            image[sr][sc] = color
+            fill(sr + 1, sc, color, previous)
+            fill(sr - 1, sc, color, previous)
+            fill(sr, sc + 1, color, previous)
+            fill(sr, sc - 1, color, previous)
+
+        fill(sr, sc, color, previous)
+        return image
+
+
+image = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+sr = 1
+sc = 1
+color = 2
+
+print(Solution().floodFill(image, sr, sc, color))
